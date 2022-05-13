2ed6a8215737e1f5625d5a8a54e1428ec41a70b4
Shannen Bendeck <shay@macbook-pro.myfiosgateway.com>
2022-05-13 00:36:30 -0400

    confirms

--- script.js
+++ script.js
@@ -15,19 +15,35 @@ function generatePassword() {
   upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   special = "!@#$%^&*()_",
   finalPassword = [""]
-  endPassword = ""\
+  endPassword = "",
 
- )};
-
-
- var passwordLength = math.floor( 
+};
+var passwordLength = math.floor( 
    prompt("How long do you want the password to be? Password must be between 8 to 128 characters");
 
-  if (number.NAN(passwordLength) && passwordLength >= 8 && passwordLength <=128) {
+if (number.NAN(passwordLength) && passwordLength >= 8 && passwordLength <=128){
+  alert("Must be between 8 and 128 characters!)
 
-    var charspecial = confirm("Do you want special characters?")
-    var char
-  }
+ var charNumbers = confirm("Do you want number characters?");
+ var charLowercase = confirm("Do you want lowercase characters?");
+ var charUppercase = confirm("Do you want uppercase characters?");
+ var charspecial = confirm("Do you want special characters?");
+
+ if (
+   !charNumbers &&
+   !charLowercase &&
+   !charUppercase &&
+   !charspecial &&
+
+ ){ 
+alert ("you have to choose at least one option!")
+var continue = confirm('Do you still want to gerate a password?"); 
+if 
+
+
+ }
+    
+  
 
 
 
