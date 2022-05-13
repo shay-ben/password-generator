7fc342568b0b28f15081b23020cd3574a4f2b9de
Shannen Bendeck <shay@macbook-pro.myfiosgateway.com>
2022-05-12 23:59:51 -0400

    legth

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-document.generateBtn.addEventListener("click". writePassword);
+generateBtn.addEventListener("click". writePassword);
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
  passwordText.value = password;
 }
- 
-var numbers = "123456789";
-var lowerCase = "abcdefghijklmkopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var special = "!@#$%^&*()_"; 
 
 function generatePassword() {
+ var characters = {
+  numbers = "123456789",
+  lowerCase = "abcdefghijklmkopqrstuvwxyz",
+  upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  special = "!@#$%^&*()_",
+  finalPassword = [""]
+  endPassword = ""\
+
+ )};
+
+
+ var passwordLength = math.floor( 
+   prompt("How long do you want the password to be? Password must be between 8 to 128 characters");
+
+  if (number.NAN(passwordLength) && passwordLength >= 8 && passwordLength <=128) {
+
+    var charspecial = confirm("Do you want special characters?")
+    var char
+  }
+
+
+
+
+
+
 
 
 
