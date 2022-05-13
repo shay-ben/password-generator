02508c668ee90ce03d9526241009c81208e30991
Shannen Bendeck <shay@macbook-pro.myfiosgateway.com>
2022-05-12 23:26:55 -0400

    ls var

---
+++ script.js
@@ -0,0 +1,26 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+document.generateBtn.addEventListener("click". writePassword);
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+ passwordText.value = password;
+}
+ 
+var numbers = "123456789";
+var lowerCase = "abcdefghijklmkopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var special = "!@#$%^&*()_"; 
+
+function generatePassword() {
+
+
+
+ 
+  return"myc00lpassword"
+
+  }
+
